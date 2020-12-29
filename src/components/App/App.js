74074c8de9bb0bb731927db273a0226af26923bd
Crookinator <a_crook@icloud.com>
2020-12-29 18:33:42 -0500

    missed a few files
    Removed some files that were missed in the original clean up.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,53 +1,14 @@
-import React, { Component, Fragment } from 'react'
-import { Route } from 'react-router-dom'
+import React, { Fragment } from 'react'
+// import { Route } from 'react-router-dom'
 
-import AutoDismissAlert from '../AutoDismissAlert/AutoDismissAlert'
 import Header from '../Header/Header'
 
-class App extends Component {
-  constructor () {
-    super()
-
-    this.state = {
-      user: null,
-      msgAlerts: []
-    }
-  }
-
-  setUser = user => this.setState({ user })
-
-  clearUser = () => this.setState({ user: null })
-
-  msgAlert = ({ heading, message, variant }) => {
-    this.setState({ msgAlerts: [...this.state.msgAlerts, { heading, message, variant }] })
-  }
-
-  render () {
-    const { msgAlerts, user } = this.state
-
-    return (
-      <Fragment>
-        <Header user={user} />
-        {msgAlerts.map((msgAlert, index) => (
-          <AutoDismissAlert
-            key={index}
-            heading={msgAlert.heading}
-            variant={msgAlert.variant}
-            message={msgAlert.message}
-          />
-        ))}
-        <main className="container">
-          <Route path='/sign-up' render={() => (
-            <SignUp msgAlert={this.msgAlert} setUser={this.setUser} />
-          )} />
-          <Route path='/sign-in' render={() => (
-            <SignIn msgAlert={this.msgAlert} setUser={this.setUser} />
-          )} />
-
-        </main>
-      </Fragment>
-    )
-  }
+const App = () => {
+  return (
+    <Fragment>
+      <Header/>
+    </Fragment>
+  )
 }
 
 export default App
