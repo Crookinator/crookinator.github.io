d6aee4d3fbb7129c3312626fff848360a0d6a5f3
Crookinator <a_crook@icloud.com>
2021-01-09 20:57:46 -0500

    added changes to the carousel
    added links for the deployed sites as well as links to the client and api repos. I lengthened the interval for each slide so now each stays in view for 10 seconds.

--- src/components/profile/Projects.js
+++ src/components/profile/Projects.js
@@ -12,15 +12,15 @@ const Projects = () => (
   <div className='row'>
     <div className='col-sm-10 col-md-8 mx-auto mt-5'>
       <h2>My Projects</h2>
-      <Carousel>
+      <Carousel interval={10000}>
         <Carousel.Item>
           <img
             className="d-block w-100"
             src={batman}
             alt="Screenshot of Batman tic-tac-toe. The game board has the bat cave behind it and the colors are gray, black, and yellow."
           />
-          <h3>Batman Tic-Tac-Toe</h3>
-          <p className='carouselText'>For my first project I created a 1990s Batman themed tic-tac-toe game. This game authenticates a user, stores there game data to an api and includes audio tracks when the user successfully logs in and when either X or O wins a game.</p>
+          <h3><a href="https://crookinator.github.io/BATMAN-tic-tac-toe/" target='_blank' rel='noopener noreferrer'>Batman tic-tac-toe</a></h3>
+          <p className='carouselText'>For my first project I created a 1990s Batman themed tic-tac-toe game. This game authenticates a user, stores there game data to an api and includes audio tracks when the user successfully logs in and when either X or O wins a game. Check out the <a href="https://github.com/Crookinator/BATMAN-tic-tac-toe" target='_blank' rel='noopener noreferrer'>repo</a> on GitHub.</p>
         </Carousel.Item>
         <Carousel.Item>
           <img
@@ -28,8 +28,8 @@ const Projects = () => (
             src={gdl}
             alt="A screenshot of the Guide Dog Log interface. The background is black with white text and a green header. "
           />
-          <h3>Guide Dog Log</h3>
-          <p className='carouselText'>My second project focuses more on solving a need. As a guide dog handler myself I found a need for a forum where guide dog handlers could share experiences. I built the client side and the api apps as well as included some helpful information to visitors to the site. This full stack app also requires user authentication.</p>
+          <h3><a href="https://crookinator.github.io/guide-dog-log-client/" target='_blank' rel='noopener noreferrer'>Guide Dog Log</a></h3>
+          <p className='carouselText'>My second project focuses more on solving a need. As a guide dog handler myself I found a need for a forum where guide dog handlers could share experiences. I built the client side and the api apps as well as included some helpful information to visitors to the site. This full stack app also requires user authentication. Check out the <a href="https://github.com/Crookinator/guide-dog-log-client" target='_blank' rel='noopener noreferrer'>client repo</a> and the <a href="https://github.com/Crookinator/guide-dog-log-api" target='_blank' rel='noopener noreferrer'>api repo</a>.</p>
         </Carousel.Item>
         <Carousel.Item>
           <img
@@ -37,8 +37,8 @@ const Projects = () => (
             src={scrumiLight}
             alt="A screenshot of the scrumi app. The interface has a purple navigation bar at the top with a white body and black text."
           />
-          <h3>Scrumi</h3>
-          <p className='carouselText'>This third project was a group project utilizing React. We wanted to create a scrum focused to do list. This to do list has both individual tasks as well as sprints. Tasks and Sprints are their own resources on the api so you can have a task without needing to assign a sprint to it.</p>
+          <h3><a href="https://pojo-boys.github.io/Scrumi/#/" target='_blank' rel='noopener noreferrer'>Scrumi</a></h3>
+          <p className='carouselText'>This third project was a group project utilizing React. We wanted to create a scrum focused to do list. This to do list has both individual tasks as well as sprints. Tasks and Sprints are their own resources on the api so you can have a task without needing to assign a sprint to it. Check out the <a href="https://github.com/Pojo-boys/Scrumi" target='_blank' rel='noopener noreferrer'>client repo</a> and the <a href="https://github.com/Pojo-boys/Api-Scrumi" target='_blank' rel='noopener noreferrer'>api repo</a>.</p>
         </Carousel.Item>
         <Carousel.Item>
           <img
@@ -46,7 +46,7 @@ const Projects = () => (
             src={scrumiDark}
             alt="A screenshot of the scrumi dark mode app. The interface has a purple navigation bar at the top with a black background and white text."
           />
-          <h3>Scrumi (dark mode)</h3>
+          <h3><a href="https://pojo-boys.github.io/Scrumi/#/" target='_blank' rel='noopener noreferrer'>Scrumi</a> (dark mode)</h3>
           <p className='carouselText'>With this project I also integrated an @media query for when users have system wide dark mode enabled. When this is true the entire app interface changes to accomidate the users preference.</p>
         </Carousel.Item>
         <Carousel.Item>
@@ -55,8 +55,8 @@ const Projects = () => (
             src={kkLight}
             alt="The find recipes view of Kidz Kitchen. The Navigation bar and main items have a very kid friendly orange color, while the background is white."
           />
-          <h3>Kidz Kitchen</h3>
-          <p className='carouselText'>My most recent project came about because my son is very into cooking right now and asked me to create an app where he could keep track of recipes as well as get recipe ideas. This app is also full stack, and includes a dark mode version. I used Python, Django, and PostgreSQL to create the api. I used React for the client side application. I also connected to a third party api called recipe puppy for the recipe suggestions.</p>
+          <h3><a href="https://crookinator.github.io/kidz-kitchen/#/" target='_blank' rel='noopener noreferrer'>Kidz Kitchen</a></h3>
+          <p className='carouselText'>My most recent project came about because my son is very into cooking right now and asked me to create an app where he could keep track of recipes as well as get recipe ideas. This app is also full stack, and includes a dark mode version. I used Python, Django, and PostgreSQL to create the api. I used React for the client side application. I also connected to a third party api called recipe puppy for the recipe suggestions. Check out the <a href="https://github.com/Crookinator/kidz-kitchen" target='_blank' rel='noopener noreferrer'>client repo</a> and the <a href="https://github.com/Crookinator/kidz-kitchen-api" target='_blank' rel='noopener noreferrer'>api repo</a>.</p>
         </Carousel.Item>
         <Carousel.Item>
           <img
@@ -64,7 +64,7 @@ const Projects = () => (
             src={kkDark}
             alt="The find recipes view of Kidz Kitchen. This is the dark mode version so the background is black and the navigation bar and on screen items have a very dark gray background. All the text is white."
           />
-          <h3>Kidz Kitchen (dark mode)</h3>
+          <h3><a href="https://crookinator.github.io/kidz-kitchen/#/" target='_blank' rel='noopener noreferrer'>Kidz Kitchen</a> (dark mode)</h3>
           <p className='carouselText'>Knowing that my son chose a very jaring orange for the light mode version of the app. I strove to create a very muted UX for the dark mode. This dark mode is achieved the same as the Scrumi dark mode. I used an @media query to check to see if the user had system wide dark mode enabled.</p>
         </Carousel.Item>
       </Carousel>    </div>
